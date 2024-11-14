e539547aa9e50203c2f2cbdbc396e8e12554d8ef
Ayuro <ayuroko@gmail.com>
2024-11-14 17:42:22 +0100

    feat: basic implementations

---
+++ server.js
@@ -0,0 +1,52 @@
+/** Server Imports */
+import express from 'express';
+import cors from 'cors';
+
+/** MongoDB Imports */
+import mongoose from 'mongoose';
+
+/** Init DB */
+const dataBase = mongoose();
+mongoose.connect('mongodb://localhost:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to Mongo');
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+/** Init server */
+const server = express();
+const port = 1986;
+server.use(cors());
+
+/** Root management routes */
+
+server.get('/', (req, res, next) => {
+    console.log("Am I awake?");
+    return res.send();
+});
+
+/** Account management routes */
+
+server.get('/register', (req, res, next) => {
+    console.log("Am I about to sign something?");
+    return res.send();
+})
+
+server.get('/login', (req, res, next) => {
+    console.log("Honey, I'm home!");
+    return res.send();
+})
+
+server.get('/logout', (req, res, next) => {
+    console.log("I'm just getting cigarettes, it will take 10mins");
+    return res.send();
+})
+
+/** Server listening */
+
+server.listen(1986, () => {
+    console.log(`Server listening on ${port}`);
+})
